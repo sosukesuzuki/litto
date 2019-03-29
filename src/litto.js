b4b8d976658d5c60b79cc365635acef48c0c9827
Sosuke Suzuki <aosukeke@gmail.com>
2019-03-30 01:10:47 +0900

    Implement markdown parser

--- src/litto.js
+++ src/litto.js
@@ -1,6 +1,15 @@
 "use strict";
 
+const unified = require("unified");
+const markdown = require("remark-parse");
+
+function parse(text) {
+  const processor = unified().use(markdown, { commonmark: true });
+  return processor.runSync(processor.parse(text));
+}
+
 function litto(text) {
+  console.log(parse(text));
   return text;
 }
 
