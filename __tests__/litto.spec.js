bbee3102d431394b2425205fa94bcb3848be060e
Sosuke Suzuki <aosukeke@gmail.com>
2019-03-31 01:33:21 +0900

    Add test

--- __tests__/litto.spec.js
+++ __tests__/litto.spec.js
@@ -3,8 +3,21 @@
 const litto = require("../src/litto").litto;
 
 describe("litto", () => {
-  test("identify", () => {
-    const formatted = litto("hoge");
-    expect(formatted).toBe("hoge");
+  test("convert list to checklist", () => {
+    const content = "- item1\n- item2\n- item3\n";
+    const result = litto(content);
+    expect(result).toBe("-   [ ] item1\n-   [ ] item2\n-   [ ] item3\n");
+  });
+  test("convert list in list to checklist in checklist", () => {
+    const content = "- item1\n  - subitem1\n  - subitem2\n";
+    const result = litto(content);
+    expect(result).toBe(
+      "-   [ ] item1\n    -   [ ] subitem1\n    -   [ ] subitem2\n"
+    );
+  });
+  test("convert list to checklist with prettier", () => {
+    const content = "- item1\n- item2\n- item3\n";
+    const result = litto(content, { withFormat: true });
+    expect(result).toBe("- [ ] item1\n- [ ] item2\n- [ ] item3\n");
   });
 });
